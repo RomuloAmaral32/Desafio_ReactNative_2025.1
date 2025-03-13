f49a916ab63bef6fb22e2dbfa38fd31336c86f1f
RomuloAmaral <romuloamaral32@gmail.com>
2025-03-13 16:08:00 -0300

    faz a adicção de imagens do dispositivo ao criar e editar

--- components/ModalCriar.tsx
+++ components/ModalCriar.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 interface Product {
   id: string; 
@@ -24,6 +26,25 @@ const ModalCriarProduto: React.FC<ModalCreateProductProps> = ({ visible, onClose
     category: '',
   });
 
+  // Função para abrir a galeria e escolher uma imagem
+  const pickImage = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted) {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setNewProduct((prev) => ({ ...prev, imageUri: result.assets[0].uri }));
+      }
+    } else {
+      alert('Permissão para acessar a galeria foi negada');
+    }
+  };
+
   const handleChange = (field: keyof Product, value: string) => {
     setNewProduct((prev) => ({ ...prev, [field]: value }));
   };
@@ -31,7 +52,7 @@ const ModalCriarProduto: React.FC<ModalCreateProductProps> = ({ visible, onClose
   const handleCreate = () => {
     onCreate(newProduct); // Envia o novo produto para a função onCreate
     onClose(); // Fecha o modal
-    setNewProduct({id: '', imageUri: '', title: '', price: '', category: '' }); // Limpa os campos após criar
+    setNewProduct({ id: '', imageUri: '', title: '', price: '', category: '' }); // Limpa os campos após criar
   };
 
   return (
@@ -39,6 +60,14 @@ const ModalCriarProduto: React.FC<ModalCreateProductProps> = ({ visible, onClose
       <View style={styles.modalBackground}>
         <View style={styles.modalContainer}>
           <Text style={styles.modalTitle}>Criar Produto</Text>
+          <TouchableOpacity onPress={pickImage} style={styles.imageContainer}>
+            {newProduct.imageUri ? (
+              <Image source={{ uri: newProduct.imageUri }} style={styles.image} />
+            ) : (
+              <MaterialCommunityIcons name="image-plus" size={48} color="black" />
+            )}
+          </TouchableOpacity>
+
           <TextInput
             style={styles.input}
             placeholder="Título"
@@ -58,21 +87,15 @@ const ModalCriarProduto: React.FC<ModalCreateProductProps> = ({ visible, onClose
             value={newProduct.price}
             onChangeText={(text) => handleChange('price', text)}
           />
-          <TextInput
-            style={styles.input}
-            placeholder="Imagem (URL)"
-            value={newProduct.imageUri}
-            onChangeText={(text) => handleChange('imageUri', text)}
-          />
 
-                <View style={styles.buttons}>
-                    <TouchableOpacity onPress={onClose} style={styles.button}>
-                    <Text style={styles.buttonText}>Cancelar</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={handleCreate} style={styles.button}>
-                    <Text style={styles.buttonText}>Criar</Text>
-                    </TouchableOpacity>
-                </View>
+          <View style={styles.buttons}>
+            <TouchableOpacity onPress={onClose} style={styles.button}>
+              <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleCreate} style={styles.button}>
+              <Text style={styles.buttonText}>Criar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </Modal>
@@ -106,10 +129,27 @@ const styles = StyleSheet.create({
     borderColor: '#ddd',
     borderRadius: 5,
   },
+  imageContainer: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderRadius: 8,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 8,
+  },
+  imageText: {
+    color: '#888',
+  },
   buttons: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: '100%',
+   gap: 40,
   },
   button: {
     backgroundColor: '#2028BF',
--- components/ModalEditar.tsx
+++ components/ModalEditar.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
-import { Modal, View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
-//import { Product } from '../components/ModalGenerico';
-
-
+import { Modal, View, Text, TextInput, Button, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { Product } from '../app/(tabs)/controle';
+import * as ImagePicker from 'expo-image-picker';
 
 interface ModalEditarProdutoProps {
   visible: boolean;
@@ -12,7 +11,6 @@ interface ModalEditarProdutoProps {
   onSave: (updatedProduct: Product) => void;
 }
 
-  
 const ModalEditarProduto: React.FC<ModalEditarProdutoProps> = ({ visible, onClose, product, onSave }) => {
   const [editedProduct, setEditedProduct] = useState<Product | null>(product);
 
@@ -21,14 +19,32 @@ const ModalEditarProduto: React.FC<ModalEditarProdutoProps> = ({ visible, onClos
     setEditedProduct(product);
   }, [product]);
 
+  // Função para escolher a imagem
+  const pickImage = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted) {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      if (!result.canceled && result.assets && result.assets.length > 0) {
+        const selectedImageUri = result.assets[0].uri;
+        setEditedProduct((prevProduct) => prevProduct ? { ...prevProduct, imageUri: selectedImageUri } : null);
+      }
+    } else {
+      alert('Permissão para acessar a galeria foi negada');
+    }
+  };
+
   // Função para salvar as alterações
   const handleSave = () => {
     if (editedProduct) {
       onSave(editedProduct); // Chama a função de salvar com o produto editado
     }
   };
-  
-  
 
   return (
     <Modal
@@ -43,7 +59,19 @@ const ModalEditarProduto: React.FC<ModalEditarProdutoProps> = ({ visible, onClos
 
           {editedProduct && (
             <>
-
+              {product && (
+                <View style={styles.imageContainer}>
+                  <ImageBackground
+                    style={styles.image}
+                    source={{ uri: editedProduct.imageUri }}
+                    blurRadius={3}
+                  >
+                    <TouchableOpacity style={styles.iconContainer} onPress={pickImage}>
+                      <Ionicons name="pencil" size={50} color="#fff" />
+                    </TouchableOpacity>
+                  </ImageBackground>
+                </View>
+              )}
               <TextInput
                 style={styles.input}
                 placeholder="Título"
@@ -62,17 +90,15 @@ const ModalEditarProduto: React.FC<ModalEditarProdutoProps> = ({ visible, onClos
                 value={editedProduct.category}
                 onChangeText={(text) => setEditedProduct({ ...editedProduct, category: text })}
               />
-              
             </>
           )}
           <View style={styles.buttonContainer}>
-          <TouchableOpacity style={[styles.button, styles.cancelButton]} onPress={onClose}>
-            <Text style={styles.buttonText}>Cancelar</Text>
-          </TouchableOpacity>
-              <TouchableOpacity style={styles.button} onPress={handleSave}>
-                <Text style={styles.buttonText}>Salvar</Text>
-              </TouchableOpacity>
-
+            <TouchableOpacity style={[styles.button, styles.cancelButton]} onPress={onClose}>
+              <Text style={styles.buttonText}>Cancelar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleSave}>
+              <Text style={styles.buttonText}>Salvar</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -125,6 +151,25 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
+  },
+  imageContainer: {
+    marginBottom: 20,
+    width: 200,
+    height: 200,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)', // Fundo escuro semi-transparente
+    borderRadius: 50,
+    padding: 10,
   },
 });
 
